d9a439cadd175107313b1e388db0254b70042942
Bishesh5673 <bisesmhrzn101@gmail.com>
2025-12-19 19:50:09 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div> 
+      App
+    </div>
+  )
+}
+
+export default App
+
+// framework->library->package->module->function
+// React is a Library
+// SPA + Component based arc
+// SPA->single html page/index.html
+// SPA passes JSon instead of html file which is much faster, no reload
+// multi page passes html file on each new request
+
+// npm create vite@latest .
+// npm-> node pacckage manager / registryfor JS
+// vite-> installs npm fast / npx is slow
